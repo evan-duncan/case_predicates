45510aa81a342ebbc342ceb454e078e8544ddd61
Evan Duncan <evan.e.duncan@gmail.com>
2021-02-09 20:47:53 -0600

    Initial commit

---
+++ lib/case_predicates.rb
@@ -0,0 +1,6 @@
+require "case_predicates/version"
+
+module CasePredicates
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/case_predicates/version.rb
@@ -0,0 +1,3 @@
+module CasePredicates
+  VERSION = "0.1.0"
+end
